86909f48cba3da3e91d1db47defbd72dfb4b2d38
bechirhaddad <bechir.haddad@esprit.tn>
2023-06-24 12:16:11 +0100

    erreur corrigée : logger

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
+import java.util.logging.*;
+
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        private static final Logger logger = Logger.getLogger(HelloWorld.class.getName())
+        ConsoleHandler consoleHandler = new ConsoleHandler();
+        consoleHandler.setLevel(Level.INFO);
+        logger.addHandler(consoleHandler);
+        logger.info("Hello World .");
     }
 }
